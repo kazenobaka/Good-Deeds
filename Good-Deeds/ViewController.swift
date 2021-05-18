0007c348d00f6de325644c77bb3235ddd2ad95e3
Алексей <aleksey.pakaa@gmail.com>
2021-05-18 03:42:17 -0700

    Initial Commit

---
+++ Good-Deeds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Good-Deeds
+//
+//  Created by Алексей on 18.05.2021.
+//  Copyright © 2021 Татьяна. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
